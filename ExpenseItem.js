263a563cd6ead97fd7b7bba849a786f211a2cada
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-03-23 16:15:16 +0530

    File added

---
+++ ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return (<div>
+        <h2>Expense Items</h2>
+        <p>Foor Rs 10</p>
+        <p>Patrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>)
+}
+
+export default ExpenseItem;
