fc442b035018bd3c7864416877193dde8b3e3a67
AkashDY <89009592+AkashDY@users.noreply.github.com>
2023-04-02 15:27:19 +0530

    text after list added

---
+++ updatedProject/src/components/Expenses/ExpensesList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = props => {
+    if (props.items.length === 0){
+        return <h2 className='expenses-list__fallback'>Found no expenses</h2>
+    }
+
+    if (props.items.length === 1){
+        return <div>
+            <ul className='expenses-list'>
+            <ExpenseItem
+                    key={props.items[0].id}
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+            </ul>
+            <h2 className='expenses-list__fallback'>
+                Only single Expense here. Add more...
+            </h2>
+        </div>
+    }
+
+    return (
+        <ul className='expenses-list'>
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default ExpensesList;
